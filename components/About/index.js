c87687c26df2c1a6044c243071e89662f4a34b15
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2021-12-14 16:31:57 +0530

    changes added

--- components/About/index.js
+++ components/About/index.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiInstagram, FiLinkedin, FiTwitter, FiGithub, FiLayers, FiEdit2 } from 'react-icons/fi';
 
 export default function About() {
-    return (
-        <section style={{height:'100vh'}} id='about-section'>
-            This is about me
-        </section>
-    )
+  return (
+    <section className="section" style={{ height: "100vh" }} id="about-section">
+      <div className="container">
+        <div className="row mobile-row-reverse d-flex no-gutters">
+          <div className="col-md-6 col-lg-6 img-column">
+            <div className="unset-img img-about">
+              <Image src={"/me.jpeg"} alt='aaquib ahmed' layout="fill" className="custom-img" />
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-6 pl-md-5 py-5">
+            <div className="row justify-content-start pb-3">
+              <div className="col-md-12 heading-section ftco-animate fadeInUp ftco-animated">
+                <h2 className="mb-4">About Me</h2>
+                <p>
+                  Hi 👋 Everyone, I am a frontend engineer with 2 years of
+                  experience in developing and shipping appealing websites and
+                  web apps. Earlier I was working as a frontend developer in a
+                  dynamic startup, where I built a B2B Dashboard webapp, and add
+                  new features like appointment booking, analytics graph,
+                  setting up admin roles, etc, thus single-handedly completing
+                  the app to 80%. I had done my engineering at Bangalore
+                  Institute of Technology, Bengaluru.
+                </p>
+                <ul className="about-info mt-4 px-md-0 px-2">
+                  <li className="d-flex">
+                    <span>Name:</span>
+                    <span>Aaquib Ahmed</span>
+                  </li>
+                  <li className="d-flex">
+                    <span>Email:</span>
+                    <span>aaquib5076@gmail.com</span>
+                  </li>
+                </ul>
+                <ul className="about-social mt-4 px-md-0 px-2">
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiInstagram />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiLinkedin />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiTwitter />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiGithub />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiLayers />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a>
+                        <FiEdit2 />
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+{
+  /* <Image className='img d-flex align-self-stretch align-items-center' 
+                            src='/me.jpg' alt='aaquib ahmed' layout='fill'/> */
 }
--- components/Home/index.js
+++ components/Home/index.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import { AiOutlineThunderbolt } from 'react-icons/ai'
+import React, { useEffect } from "react";
+import MultiTasking from "../../helpers/multi-tasking.json";
+import Lottie from "react-lottie";
+import { AiOutlineThunderbolt } from "react-icons/ai";
 
 export default function HomeSection() {
-    return (
-        <section style={{height:'700px'}} className='section py-5 hero-wrap' id='home-section'>
-            <div className='overlay'></div>
-            <div className='container'>
-                <div style={{height:'700px'}} className='row no-gutters justify-content-center align-items-center'>
-                    <div className='col-md-6 col-sm-12'>col 1</div>
-                    <div className='col-md-6 col-sm-12'>col 2</div>
-                </div>
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: MultiTasking,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  useEffect(() => {
+    //Type-writer code
+    (function () {
+      const text = ["developer", "blogger", "designer"];
+      let count = 0;
+      let index = 0;
+      let currentText = " ";
+      let letter = " ";
+
+      (function type() {
+        if (count === text.length) {
+          count = 0;
+        }
+        currentText = text[count];
+        letter = currentText.slice(0, ++index);
+
+        document.querySelector(".typing").textContent = letter;
+        if (letter.length === currentText.length) {
+          count++;
+          index = 0;
+        }
+        setTimeout(type, 300);
+      })();
+    })();
+  }, []);
+
+  return (
+    <section
+      style={{ height: "100vh" }}
+      className="section py-5 hero-wrap"
+      id="home-section"
+    >
+      <div className="overlay"></div>
+      <div className="container">
+        <div
+          style={{ height: "700px" }}
+          className="row no-gutters justify-content-center align-items-center"
+        >
+          <div className="col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+            <div className="text text-center">
+              <span className="subheading">Hey! I am</span>
+              <h1 className="heading">Aaquib Ahmed</h1>
+              <div className="intro">
+                <h4>I am a</h4>
+                <h4 className="typing">hi</h4>
+              </div>
             </div>
-            <div className='mouse'>
-                <div className='mouse-icon'>
-                    <div className='mouse-wheel'>
-                        <AiOutlineThunderbolt/>
-                    </div>
-                </div>
+          </div>
+          <div className="col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+              <div className='loftiplayer'>
+                <Lottie options={defaultOptions} />
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+      <div className="mouse">
+        <div className="mouse-icon">
+          <div className="mouse-wheel">
+            <AiOutlineThunderbolt />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
