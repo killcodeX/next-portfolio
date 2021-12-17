d12c3c0c399eccd4f3e484b37f59b3ed1caf6877
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2021-12-17 21:38:27 +0530

    contact section complete

--- components/Contacts/index.js
+++ components/Contacts/index.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import Image from "next/image";
 
 export default function Contacts() {
-    return (
-        <section className='section heading-section' id='contact-section'>
-            <h2 className='mb-4'>Contacts</h2>
-            <div className='container'>Contact Section</div>
-        </section>
-    )
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = () => {
+      e.preventDefault();
+    alert('Message Sent :)')
+  };
+
+  return (
+    <section className="section heading-section" id="contact-section">
+      <h2 className="mb-4">Contacts</h2>
+      <div className="container">
+        <div className="row mobile-row-reverse">
+          <div className="col-sm-12 col-md-6 d-flex justify-content-center align-items-center">
+            <form className="contact-form" onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <input
+                  placeholder="Full Name"
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  value={name}
+                  aria-describedby="name"
+                  onChange={() => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  placeholder="Email Address"
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  value={email}
+                  aria-describedby="emailHelp"
+                  onChange={() => setEmail(e.target.value)}
+                />
+                <div id="emailHelp" class="form-text">
+                  I'll never share your email with anyone else.
+                </div>
+              </div>
+              <div className="mb-3">
+                <input
+                  placeholder="Your Message"
+                  type="textarea"
+                  class="form-control"
+                  id="message"
+                  value={message}
+                  aria-describedby="emailHelp"
+                  onChange={() => setMessage(e.target.value)}
+                />
+              </div>
+              <button type='submit' className='contact-btn'>Send Message</button>
+            </form>
+          </div>
+          <div className="col-sm-12 col-md-6 d-flex justify-content-center align-items-center">
+            <div className="unset-img img-contact">
+              <Image
+                src={"/contact-us.png"}
+                alt="contact"
+                layout="fill"
+                className="custom-img"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
