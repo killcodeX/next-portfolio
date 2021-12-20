844abd8b17585da2f3e6267840c64125d38aaa87
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2021-12-20 11:03:29 +0530

    tried parrallax

--- components/skills/index.js
+++ components/skills/index.js
@@ -1,32 +1,44 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Parallax } from 'react-scroll-parallax';
-import { DiJavascript1, DiCss3Full, DiReact, DiNodejs } from "react-icons/di";
+import React from "react";
+import { Parallax } from "react-scroll-parallax";
+import {
+  DiBootstrap,
+  DiJavascript1,
+  DiCss3Full,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPhotoshop,
+  DiPython,
+  DiSass
+} from "react-icons/di";
 
 export default function Skills() {
   return (
     <section className="section heading-section" id="skill-section">
       <h2 className="mb-4">Skills</h2>
-      <div className="parallax-outer">
-        <Parallax className="parallax-inner" y={[-20, 20]} tagOuter="figure">
-          <span className="skill-1">bla</span>
-          <span className="skill-2">
-            <DiJavascript1 />
-          </span>
-          <span className="skill-3">
-            <DiCss3Full />
-          </span>
-          <span className="skill-4">
-            <DiReact />
-          </span>
-          <span className="skill-5">
-            <DiNodejs />
-          </span>
-          <span className="skill-6">6</span>
-          <span className="skill-7">7</span>
-          <span className="skill-8">8</span>
-          <span className="skill-9">9</span>
-        </Parallax>
-      </div>
+      <Parallax offsetYMin={0} offsetYMax={20}>
+        <span className="skill-1">
+          <DiBootstrap />
+        </span>
+        <span className="skill-2">
+          <DiMongodb />
+        </span>
+        <span className="skill-3">
+          <DiPhotoshop />
+        </span>
+        <span className="skill-4">
+          <DiPython/>
+        </span>
+        <span className="skill-5">
+          <DiSass/>
+        </span>
+        <span className="skill-6">antd</span>
+        <span className="skill-7">
+          <DiMongodb />
+        </span>
+        <span className="skill-8">8</span>
+        <span className="skill-9">9</span>
+      </Parallax>
       <div className="container">
         <div className="row skills-height">
           <div className="col-sm-3 d-flex justify-content-center align-items-center">
@@ -51,7 +63,6 @@ export default function Skills() {
           </div>
         </div>
       </div>
-      {/*  */}
     </section>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 import "../styles/globals.css";
-import '../styles/header.css';
-import '../styles/sidebar.css';
-import '../styles/home.css';
-import '../styles/about.css';
-import '../styles/skills.css';
-import '../styles/resume.css';
-import '../styles/projects.css';
-import '../styles/blogs.css';
-import '../styles/contacts.css';
-import '../styles/footer.css';
+import "../styles/header.css";
+import "../styles/sidebar.css";
+import "../styles/home.css";
+import "../styles/about.css";
+import "../styles/skills.css";
+import "../styles/resume.css";
+import "../styles/projects.css";
+import "../styles/blogs.css";
+import "../styles/contacts.css";
+import "../styles/footer.css";
 import { ThemeProvider } from "next-themes";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ParallaxProvider>
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ParallaxProvider>
   );
 }
 
