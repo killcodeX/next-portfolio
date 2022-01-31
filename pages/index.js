96927d9f4fb99522bb5ea916a154c885dfa59b62
Aaquib Ahmed <aaquibskimbox@gmail.com>
2022-02-01 00:12:20 +0530

    meta tag updated

--- pages/index.js
+++ pages/index.js
@@ -23,57 +23,26 @@ export default function Home() {
   return (
     <>
       <Head>
-        {/* primary */}
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        {/* primary */}
         <meta name="author" content="Aaquib Ahmed, aaquib5076@gmail.com" />
-        <meta
-          name="keywords"
-          content="Aaquib Ahmed,Aaquib,Tech Enthusiast,Web Developer,Portfolio, React Developer, Front-End Developer"
-        />
-        <meta property="og:site_name" content="Aaquib Ahmed Portfolio" />
-        <meta property="og:url" content="aaquib.netlify.app" />
-        <meta property="og:title" content="Aaquib Ahmed Portfolio" />
-        <meta property="og:image" content="/me.jpeg" />
-        <meta
-          property="og:description"
-          content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."
-        />
-        <meta
-          name="keywords"
-          content="Aaquib Ahmed,Aaquib,Tech Enthusiast,Web Developer,Portfolio, React Developer, Front-End Develope"
-        />
-        <link rel="icon" href="/fav2.png" />
-
-        <title>Aaquib Ahmed | Software Engineer</title>
-        <meta name="title" content="Aaquib Ahmed | Software Engineer" />
-        <meta
-          name="description"
-          content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."
-        />
-        <meta property="og:type" content="website" />
+        <meta name="title" content="Aaquib Ahmed | Software Engineer"/>
+        <meta name="description" content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."/>
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website"/>
         <meta property="og:url" content="https://aaquib.netlify.app/" />
-        <meta property="og:title" content="Aaquib Ahmed | Software Engineer" />
-        <meta
-          property="og:description"
-          content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."
-        />
-        <meta property="og:image" content="https://github.com/killcodeX/next-portfolio/blob/main/public/me.jpeg" />
-          {/* twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://aaquib.netlify.app/" />
-        <meta
-          property="twitter:title"
-          content="Aaquib Ahmed | Software Engineer"
-        />
-        <meta
-          property="twitter:description"
-          content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."
-        />
-        <meta property="twitter:image" content="https://github.com/killcodeX/next-portfolio/blob/main/public/me.jpeg"/>
+        <meta property="og:title" content="Aaquib Ahmed | Software Engineer"/>
+        <meta property="og:description" content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."/>
+        <meta property="og:image" content="https://github.com/killcodeX/next-portfolio/blob/main/public/metaimage.png"/>
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://aaquib.netlify.app/"/>
+        <meta property="twitter:title" content="Aaquib Ahmed | Software Engineer"/>
+        <meta property="twitter:description" content="Aaquib Ahmed: A passionate self-taught frontEnd web developer, a wannapreneur, tech enthusiast, open source contributor (https://github.com/killcodeX) , tech blogger (https://www.devcript.com), writer, motivator and mentor for those who want to learn programming, development and recent technologies. Being an active part of many communities."/>
+        <meta property="twitter:image" content="https://github.com/killcodeX/next-portfolio/blob/main/public/metaimage.png"/>
+        <link rel="icon" href="/fav2.png" />
+        <title>Aaquib Ahmed | Software Engineer</title>
       </Head>
       <Header />
       <main>
